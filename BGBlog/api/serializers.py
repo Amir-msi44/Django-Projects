df2415935ea56fcf18ee36c6ef709d4aef840eb2
Amirreza <amirreza.7798@gmail.com>
2024-05-22 12:40:30 +0330

    REST, add user view and permissions

--- BGBlog/api/serializers.py
+++ BGBlog/api/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from blog.models import Article
+from account.models import User
 
 class ArticleSerilizer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = '__all__'
         #exclude = ('thumbnail', 'created', 'updated')
+
+
+class UserSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
--- BGBlog/api/views.py
+++ BGBlog/api/views.py
@@ -1,9 +1,28 @@
 #from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from blog.models import Article
-from .serializers import ArticleSerilizer
+from account.models import User
+from .serializers import ArticleSerilizer, UserSerilizer
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class ArticleList(ListCreateAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerilizer
+
+
+class ArticleDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerilizer
+    lookup_field = 'slug'
+
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerilizer
+    permission_classes = (IsAdminUser,)
+
+
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerilizer
+    permission_classes = (IsAdminUser,)
